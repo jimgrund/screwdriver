e759ce4ece82562dc58bd3711eeace09daeb840d
Imgrund, Jim <jim.imgrund@gmail.com>
2018-04-12 16:56:31 -0400

    updates

--- plugins/banner/create.js
+++ plugins/banner/create.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// const boom = require('boom');
+const boom = require('boom');
 const schema = require('screwdriver-data-schema');
 // const urlLib = require('url');
 
@@ -12,13 +12,20 @@ module.exports = () => ({
         notes: 'Create a specific banner',
         tags: ['api', 'banner'],
         auth: {
-            strategies: ['token', 'banners'],
-            scope: ['user']
+            strategies: ['token'],
+            scope: ['user', '!guest']
         },
         plugins: {
             'hapi-swagger': {
                 security: [{ token: [] }]
             }
+        },
+        handler: (request, reply) => {
+            const { bannerFactory } = request.server.app;
+
+            return bannerFactory.get()
+                // something broke, respond with error
+                .catch(err => reply(boom.wrap(err)));
         },
         validate: {
             payload: schema.models.banner.create
