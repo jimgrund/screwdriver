a01e772bb099e88841571c7df5528696f7e64c6b
Tiffany Kyi <tiffanykyi@gmail.com>
2018-04-27 14:31:45 -0700

    fix: Working banner test

--- test/plugins/banner.test.js
+++ test/plugins/banner.test.js
@@ -4,34 +4,13 @@ const assert = require('chai').assert;
 const sinon = require('sinon');
 const hapi = require('hapi');
 const mockery = require('mockery');
-const testBanner = require('./data/banner.json');
 
 sinon.assert.expose(assert, { prefix: '' });
 
-const getMock = (obj) => {
-    const mock = Object.assign({}, obj);
-
-    mock.update = sinon.stub();
-    mock.toJson = sinon.stub().returns(obj);
-    mock.remove = sinon.stub();
-
-    return mock;
-};
-
-const getBannersMock = (banners) => {
-    if (Array.isArray(banners)) {
-        return banners.map(getMock);
-    }
-
-    return getMock(banners);
-};
-
-describe('banner plugin test', () => {
+describe.only('banner plugin test', () => {
+    let bannerFactoryMock;
     let plugin;
     let server;
-    let bannerFactoryMock;
-    let bannerMock;
-    const apiUri = 'http://foo.bar:12345';
 
     before(() => {
         mockery.enable({
@@ -42,15 +21,8 @@ describe('banner plugin test', () => {
 
     beforeEach((done) => {
         bannerFactoryMock = {
-            create: sinon.stub(),
-            get: sinon.stub(),
-            list: sinon.stub()
+            get: sinon.stub()
         };
-
-        bannerMock = getMock(testBanner);
-        bannerMock.remove.resolves(null);
-        bannerMock.update.resolves(bannerMock);
-        bannerFactoryMock.create.resolves(bannerMock);
 
         /* eslint-disable global-require */
         plugin = require('../../plugins/banner');
@@ -61,22 +33,21 @@ describe('banner plugin test', () => {
             bannerFactory: bannerFactoryMock
         };
         server.connection({
-            host: 'localhost',
-            port: 1234,
-            uri: apiUri
+            port: 1234
         });
 
         server.auth.scheme('custom', () => ({
-            authenticate: (request, reply) => reply.continue()
+            authenticate: (request, reply) => reply.continue({
+                credentials: {
+                    scope: ['user']
+                }
+            })
         }));
         server.auth.strategy('token', 'custom');
 
-        console.log(server);
-
-        return server.register([{
+        server.register([{
             register: plugin
         }], (err) => {
-            console.log('errrrrr');
             done(err);
         });
     });
@@ -91,27 +62,7 @@ describe('banner plugin test', () => {
         mockery.disable();
     });
 
-    it.only('registers the plugin', () => {
-        assert.isOk(server.registrations.banner);
-    });
-
-    describe('GET /banner', () => {
-        let options;
-
-        beforeEach(() => {
-            options = {
-                method: 'GET',
-                url: '/banner'
-            };
-        });
-
-        it('returns 200 for a successful yaml', () => {
-            bannerFactoryMock.list.resolves(getBannersMock(testBanner));
-
-            return server.inject(options).then((reply) => {
-                assert.equal(reply.statusCode, 200);
-                assert.deepEqual(reply.result, testBanner);
-            });
-        });
+    it('registers the plugin', () => {
+        assert.isOk(server.registrations.banners);
     });
 });
